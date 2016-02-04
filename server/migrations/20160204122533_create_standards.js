a45a729d95a88ce9155bb3ea9367630a2dd21881
Nmuta Jones <nmuta@Nmutas-MacBook-Pro.local>
2016-02-04 12:32:29 -0700

    standards migration

---
+++ server/migrations/20160204122533_create_standards.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('standards', function(table){
+    table.increments();
+    table.string('title');
+    table.integer('parent_id');
+    table.integer('user_id');
+    table.integer('topic_id');
+    table.string('topic_suffix');
+    table.boolean('active');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('standards');
+};
