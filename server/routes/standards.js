d77dc76b0c50b856cdcc99bc79501ab6545a0ab4
Nmuta Jones <nmuta1@gmail.com>
2016-02-04 20:40:47 -0700

    standards routes

--- server/routes/standards.js
+++ server/routes/standards.js
@@ -19,9 +19,22 @@ router.get('/', function(req, res, next) {
 /* GET all standards listing. */
 router.get('/:id', function(req, res, next) {
   Standards.where('id', req.params.id).fetch().then(function(stan) {
-    res.send(stan.toJSON());
+    res.json(stan.toJSON());
   }).catch(function(err) {
     console.error(err);
+    res.json({});
+  });
+});
+
+router.post('/', function(req, res, next) {
+  new Standards(req.body).save().then(function(model) {
+    res.redirect("/standards");
+  });
+});
+
+router.post('/:id/update', function(req, res, next) {
+   new Standards('id', req.params.id).save(req.body, {patch: true}).then(function(model) {
+    res.redirect("/standards");
   });
 });
 
