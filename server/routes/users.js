b1e97d6da3cc2e8886c8589f163b1f6b719b50f5
Nmuta Jones <nmuta1@gmail.com>
2016-02-07 11:10:48 -0700

    password check

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,16 +14,23 @@ router.get('/', function(req, res, next) {
 
 router.post('/login', function(req, res, next) {
   var email = req.body.email;
+  var password = req.body.password;
   var logged_in = false;
   Users.where('email',email ).fetch().then(function(usr) {
-    bcrypt.compare(email, usr.email, function(err, res) {
-      logged_in = true;
-      console.log("i logged in a user");
+    var hshed_pwd = usr.toJSON().password;
+    console.log("comparing "+password+" with "+hshed_pwd);
+    bcrypt.compare(password, hshed_pwd, function(err, res) {
+      if (res){
+        logged_in = true;
+        console.log("i logged in a user");
+      } else {
+        console.log("i rejected in a user");
+      }
     });
     res.json({logged_in: logged_in})
   }).catch(function(err) {
     console.error(err);
-    console.log("i rejected in a user");
+    console.log("error logging in a user");
     res.json({logged_in: false});
   });
 });
